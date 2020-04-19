de4d69bf17b163dc47b0cfebe3cb14c910f85951
Volha Mashnitskaya <volha.mashnitskaya@gmail.com>
2020-04-19 17:28:51 +0300

    fix: fix table displaying

--- english-for-kids/src/templates/HackerSpaceHeaders.js
+++ english-for-kids/src/templates/HackerSpaceHeaders.js
@@ -1,10 +1,10 @@
 const HackerSpaceHeaders = () => `
             <div class="statistics__category-item title wort">Word</div>
             <div class="statistics__category-item title translation">Translation</div>
-            <div class="statistics__category-item title train">Clicked at train, times</div>
-            <div class="statistics__category-item title game">Clicked at game, times</div>
+            <div class="statistics__category-item title train">Training</div>
+            <div class="statistics__category-item title game">Success</div>
             <div class="statistics__category-item title errors">Errors</div>
-            <div class="statistics__category-item title errorPercent">Error percent, %</div>  
+            <div class="statistics__category-item title errorPercent">Error percent</div>  
 `;
 
 export default HackerSpaceHeaders;
